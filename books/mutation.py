a367b1ea8ede41a483ca6a5b0be604833e4d7dcd
Younes Piro <piroyounes148@gmail.com>
2022-02-27 16:30:48 +0100

    graphql mutation for creating a new book and updating a book

---
+++ books/mutation.py
@@ -0,0 +1,59 @@
+import graphene
+from .models import Books
+from graphene_django import DjangoListField, DjangoObjectType
+
+
+class BooksType(DjangoObjectType): #serialisation the data from our model to graphql
+    class Meta:
+        model = Books
+        fields = "__all__"
+
+class BooksCreateMutation(graphene.Mutation): #creating a schema for mutation
+    class Arguments:
+        title = graphene.String(required=True)
+
+    book = graphene.Field(BooksType)
+
+    @classmethod
+    def mutate(cls, root, info, title):
+        book = Books(title=title)
+        book.save() #function in django
+        return BooksCreateMutation(book=book)
+
+#in graphql interface we use this schema 
+'''
+mutation bookmutation{
+  createBook(title: "new title 3"){
+    book{
+      title
+      id
+    }
+	}
+}
+'''        
+
+class BooksUpdateMutation(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID()
+        title = graphene.String(required=True)
+    
+    book = graphene.Field(BooksType)
+
+    @classmethod
+    def mutate(cls, root, info, title, id):
+        book = Books.objects.get(id=id)
+        book.title = title
+        book.save()
+        return BooksUpdateMutation(book=book)
+
+
+'''
+mutation bookmutation{
+  updateBook(title: "update title 3" , id: 11){
+    book{
+      title
+      id
+    }
+	}
+}
+'''
--- books/schema.py
+++ books/schema.py
@@ -1,16 +1,30 @@
-from dataclasses import fields
-import graphene 
-from graphene_django import DjangoObjectType
+import graphene
 from .models import Books
+from .mutation import BooksCreateMutation , BooksType , BooksUpdateMutation
 
-class BooksType(DjangoObjectType):
-    class Meta:
-        model = Books
-        fields = '__all__'
 
+
+# query are useful to query the data from our database
 
 class Query(graphene.ObjectType):
+
     all_books = graphene.List(BooksType)
+    single_book = graphene.Field(BooksType, id=graphene.Int()) #getting a single book my id
+
+    def resolve_all_books(root,info):
+        return Books.objects.all()
+        
+    def resolve_single_book(root,info,id):
+        return Books.objects.get(pk=id)
 
 
-schema = graphene.Schema(query=Query)
+# mutation class to modify data from our database
+class Mutation(graphene.ObjectType):
+    create_book = BooksCreateMutation.Field()
+    update_book = BooksUpdateMutation.Field()
+    
+
+
+
+# importing the schema
+schema = graphene.Schema(query=Query , mutation=Mutation)
